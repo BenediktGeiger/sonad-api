5a9ba1f1f32d4072592ff41d58ecd0012ea3fcba
Benedikt Geiger <benedikt.geiger@icloud.com>
2022-10-29 10:43:07 +0300

    add whitelisting support

--- lib/presentation/http/core/middlewares/rate-limiter.ts
+++ lib/presentation/http/core/middlewares/rate-limiter.ts
@@ -3,11 +3,26 @@ import requestIp from 'request-ip';
 
 import { CustomError } from '@lib/presentation/http/core/middlewares/error-handler';
 
+const isWhiteListed = (clientIP: string): boolean => {
+	const whiteListedIPs = String(process?.env?.IP_WHITELIST).split(',');
+
+	if (whiteListedIPs.includes(clientIP)) {
+		return true;
+	}
+
+	return false;
+};
+
 const rateLimiter = async (req: Request, res: Response, next: NextFunction) => {
 	const clientIP = requestIp.getClientIp(req);
 	const minute = new Date().getMinutes();
 
 	const key = `${clientIP}:${minute}`;
+
+	if (isWhiteListed(String(clientIP))) {
+		return next();
+	}
+
 	const hasReachedRateLimit = await req.rateLimiter.hasReachedRateLimit(key);
 
 	if (hasReachedRateLimit) {
