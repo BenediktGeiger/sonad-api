4ec48339a0e9fe92cab2487d9baf19820a159e90
Benedikt Geiger <benedikt.geiger@icloud.com>
2023-07-20 23:12:15 +0300

    skip specific urls from logging

--- lib/presentation/http/core/middlewares/request-logger.ts
+++ lib/presentation/http/core/middlewares/request-logger.ts
@@ -1,9 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
+const isSkippedUrl = (originalUrl: string): boolean => {
+	const skippedUrls = String(process?.env?.SKIPPED_URLS).split(',');
+
+	if (skippedUrls.includes(originalUrl)) {
+		return true;
+	}
+
+	return false;
+};
+
 const requestLogger = async (req: Request, res: Response, next: NextFunction) => {
 	const logger = await req.requestLogger;
 	try {
-		await logger.logRequest(req);
+		if (!isSkippedUrl(req.originalUrl)) {
+			await logger.logRequest(req);
+		}
 	} catch (err) {
 		req.logger.error({
 			message: 'unable to log request',
